20ef8df229bc710432d85e5af8e3b911dd6e1e14
Nick Kramer (Teams) <nkramer@microsoft.com>
2019-10-17 17:10:51 -0700

    Q&A tracker v1

--- Microsoft.Teams.Samples.HelloWorld.Web/Controllers/HomeController.cs
+++ Microsoft.Teams.Samples.HelloWorld.Web/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Graph;
+﻿using Microsoft.Bot.Connector;
+using Microsoft.Graph;
+using System.Linq;
 using System;
+using System.Collections.Generic;
 using System.Configuration;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
@@ -13,6 +16,31 @@ namespace Microsoft.Teams.Samples.HelloWorld.Web.Controllers
         public string teamId;
         public string channelId;
         public string message;
+    }
+
+    public class Question
+    {
+        public string MessageId;
+        public int Votes;
+        public string Text;
+        //        public bool IsAnswered;
+    }
+
+    public class QandAModel
+    {
+        public static readonly Dictionary<string, QandAModel> qAndALookup = new Dictionary<string, QandAModel>();
+
+        public List<Question> Questions = new List<Question>();
+        public Dictionary<string, bool> IsQuestionAnswered = new Dictionary<string, bool>(); // maps message id -> isAnswered
+        public string messageId = "dunno";
+        public string channelId = "dunno";
+        public string teamId = "dunno";
+        public Activity BotFirstMessage = null;
+
+        public static string Encode(string teamId, string channelId, string msgId)
+            => $"{teamId}__{channelId}__{msgId}".Replace(':', '_'); // avoid asp.net bad chars -- see https://www.hanselman.com/blog/ExperimentsInWackinessAllowingPercentsAnglebracketsAndOtherNaughtyThingsInTheASPNETIISRequestURL.aspx
+
+        public string Key => Encode(teamId, channelId, messageId);
     }
 
     public class HomeController : Controller
@@ -37,11 +65,78 @@ namespace Microsoft.Teams.Samples.HelloWorld.Web.Controllers
             string token = await GetToken();
             GraphServiceClient graph = GetAuthenticatedClient(token);
 
-            string messages = await new HttpHelpers(token).HttpGetJson($"/teams/{teamId}/channels/{channelId}/messages");
+            QandAModel model = GetModel(teamId, channelId,  "");
+            await RefreshQandA(model, graph);
+            ViewBag.MyModel = model;
+            //return View("QandAView", model);
+            return View("First", model);
+
+            //string messages = await new HttpHelpers(token).HttpGetJson($"/teams/{teamId}/channels/{channelId}/messages");
 
 
-            return View(new TeamAndChannel() { teamId = teamId, channelId = channelId,
-            message=messages});
+            //return View(new TeamAndChannel() { teamId = teamId, channelId = channelId,
+            //message=messages});
+        }
+
+        public async Task RefreshQandA(QandAModel qAndA, GraphServiceClient graph)
+        {
+            var msgs = await graph.Teams[qAndA.teamId].Channels[qAndA.channelId]
+                .Messages.Request().Top(20).GetAsync();
+            //var msgs = await graph.Teams[qAndA.teamId].Channels[qAndA.channelId]
+            //    .Messages[qAndA.messageId].Replies.Request().Top(50).GetAsync();
+
+            // merge w/ existing questions 
+            var questions =
+                from m in msgs
+                where IsQuestion(m)
+                select new Question()
+                {
+                    MessageId = m.Id,
+                    Text = m.Body.Content,
+                    Votes = m.Reactions.Count()
+                };
+            qAndA.Questions = questions.OrderByDescending(m => m.Votes).ToList();
+
+            foreach (var q in questions)
+            {
+                if (!qAndA.IsQuestionAnswered.ContainsKey(q.MessageId))
+                    qAndA.IsQuestionAnswered[q.MessageId] = false;
+            }
+
+            //await UpdateCard(qAndA);
+        }
+
+        private bool IsQuestion(ChatMessage message)
+        {
+            if (message.From.User == null)  // sender is bot
+                return false;
+            if (message.Mentions == null) // no @mention
+                return true;
+            foreach (var men in message.Mentions)
+            {
+                var app = men.Mentioned.Application;
+                if (app != null
+                    && (app.AdditionalData["applicationIdentityType"] as string) == "bot")
+                    return false;
+            }
+            return true;
+        }
+
+
+        private QandAModel GetModel(string teamId, string channelId, string messageId)
+        {
+            string key = QandAModel.Encode(teamId, channelId, messageId);
+            QandAModel model;
+            if (QandAModel.qAndALookup.ContainsKey(key))
+            {
+                model = QandAModel.qAndALookup[key];
+            }
+            else
+            {
+                model = new QandAModel() { teamId = teamId, channelId = channelId, messageId = messageId };
+                QandAModel.qAndALookup[key] = model;
+            }
+            return model;
         }
 
         private static async Task<string> GetToken()
